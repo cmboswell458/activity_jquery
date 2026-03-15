3a5fb1689323a55ab3b50e7b9db1514942c964b4
cmboswell458 <cmboswell458@mail.cfcc.edu>
2026-03-15 19:23:15 -0400

    deleted script.js code

--- js/script.js
+++ js/script.js
@@ -1,41 +1,4 @@
-$(function () {
 
-```
-// Change summary paragraph text color
-$("#summary").css("color", "blue");
-
-// jQuery Validation
-$("#contactForm").validate({
-
-    rules: {
-        name: "required",
-        email: {
-            required: true,
-            email: true
-        },
-        message: "required"
-    },
-
-    messages: {
-        name: "Please enter your name.",
-        email: "Please enter a valid email address.",
-        message: "Please enter a message."
-    },
-
-    submitHandler: function(form) {
-
-        $("#successMessage").fadeIn();
-
-        form.reset();
-
-        return false;
-
-    }
-
-});
-```
-
-});
 
 
 
